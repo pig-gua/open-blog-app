4ce8e274fc204c18ee4b4ddde5d02e5e664858a5
Pig-Gua <1270701867@qq.com>
2025-07-08 22:37:47 +0800

    feat: 项目初始化

---
+++ deploy/sql/init.sql
@@ -0,0 +1,3 @@
+-- 创建数据库
+create schema `open-blog` collate utf8mb4_bin;
+
---
+++ src/main/java/cloud/abaaba/BlogApplication.java
@@ -0,0 +1,19 @@
+package cloud.abaaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * BlogApplication
+ *
+ * @author Pig-Gua
+ * @date 2025-07-08
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+}
